a10a67923fb7b1b80e123fcaff36e0e513c5e6c3
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2023-11-07 08:47:18 -0500

    Add next-pwa to next.config.js

--- next.config.js
+++ next.config.js
@@ -3,5 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: "",
 };
+const withPWA = require('next-pwa')({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+})
 
-module.exports = nextConfig;
+module.exports = withPWA({
+  basePath: "",
+  reactStrictMode: true,
+})
