4c6ae12be8cedcd352ee2e15380c0f3525de1e97
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2023-11-08 09:44:12 -0500

    i tailwind

--- pages/api/describe.ts
+++ pages/api/describe.ts
@@ -2,56 +2,67 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { OpenAI } from "openai";
 
+// Define the structure of the message in the choices array
+interface OpenAIChoiceMessage {
+  content: string | { text: string }; // Adjust according to the actual content structure
+}
+
+// Define the structure of a choice
+interface OpenAIChoice {
+  message: OpenAIChoiceMessage;
+  index: number;
+}
+
+// Define the structure of the OpenAI API response
+interface OpenAIResponse {
+  id: string;
+  object: string;
+  created: number;
+  model: string;
+  choices: OpenAIChoice[];
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
     const { imageData } = req.body;
-    console.log("imageData", imageData);
-    const openai = new OpenAI();
-    console.log("openai", openai);
+    const openai = new OpenAI(); // Ensure the API key is passed correctly
 
     try {
       const response = await openai.chat.completions.create({
         model: "gpt-4-vision-preview",
+        max_tokens: 1000,
         messages: [
           {
             role: "user",
             content: [
-              { type: "text", text: "What’s in this image?" },
+              { type: "text", text: "whats in the image" },
               {
                 type: "image_url",
-                image_url: `data:image/jpeg;base64,${imageData}`,
+                image_url: `data:image/jpeg;base64,${imageData}` as any,
               },
             ],
           },
         ],
-      });
+      }) as OpenAIResponse; // Type assertion here
 
-      // Log the full API response
-      console.log("OpenAI response:", response);
-
-      // Check if 'choices' is present in the response
-      if (response.choices) {
-        console.log("response.choices", response.choices);
-        const message = response.choices[0].message.content
-        res
-          .status(200)
-          .json({ description: message });
+      if (response.choices && response.choices.length > 0) {
+        const choice = response.choices[0];
+        if (typeof choice.message.content === 'string') {
+          res.status(200).json({ description: choice.message.content });
+        } else {
+          res.status(200).json({ description: choice.message.content.text });
+        }
       } else {
-        // If 'choices' is not present, log the data and return an error message
-        console.error("Unexpected response structure:", response);
-        res
-          .status(500)
-          .json({ error: "Unexpected response structure from OpenAI API." });
+        res.status(500).json({ error: "No choices found in the OpenAI API response." });
       }
     } catch (error) {
       console.error("OpenAI API error:", error);
       res.status(500).json({ error: "Error communicating with OpenAI API." });
     }
   } else {
-    // Handle any other HTTP method
     res.setHeader("Allow", ["POST"]);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
--- pages/test.jsx
+++ pages/test.jsx
@@ -1,16 +1,12 @@
-import { useState, useRef, useEffect, useCallback } from "react";
+import { useState, useRef, useEffect } from "react";
 import styles from "../styles/Home.module.css";
 
-// Define a type for the video element ref
-type VideoElementRef = HTMLVideoElement | null;
-
 const Home: React.FC = () => {
-  // useRef should use the defined type and be initialized with null
-  const videoRef = useRef<VideoElementRef>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
   const [streamStarted, setStreamStarted] = useState<boolean>(false);
   const [videoDescription, setVideoDescription] = useState<string>("");
 
-  const getVideoStream = useCallback(async () => {
+  const getVideoStream = async () => {
     try {
       const stream = await navigator.mediaDevices.getUserMedia({ video: true });
       if (videoRef.current) {
@@ -20,38 +16,42 @@ const Home: React.FC = () => {
     } catch (error) {
       console.error("Error accessing the webcam:", error);
     }
-  }, []);
+  };
 
-  const captureImage = useCallback(async () => {
+  const captureImage = async () => {
     if (videoRef.current) {
       const video = videoRef.current;
-      const scaleFactor = 0.5;
-      const canvas = document.createElement("canvas");
+      const scaleFactor = 0.5; // Change this value to scale down the image size
+      const canvas = document.createElement('canvas');
       canvas.width = video.videoWidth * scaleFactor;
       canvas.height = video.videoHeight * scaleFactor;
-      const ctx = canvas.getContext("2d");
-      if (ctx) {
-        // Check if the context is not null
-        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-        const imageUrl = canvas.toDataURL("image/jpeg", 0.5);
-        console.log(`Image URL length: ${imageUrl.length}`);
-        const imageSizeKB = (imageUrl.length * (3 / 4)) / 1024;
-        console.log(`Approximate image size: ${imageSizeKB} KB`);
-
-        if (imageSizeKB > 5120) {
-          console.error("Image size exceeds 5MB");
-          return "";
-        }
-
-        console.log(`Image Data URL Sample: ${imageUrl.substring(0, 100)}`);
-        return imageUrl.replace(/^data:image\/jpeg;base64,/, "");
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+  
+      // Convert the canvas to a data URL with a specified quality
+      const imageUrl = canvas.toDataURL('image/jpeg', 0.5);
+      
+      // Log the image URL length and size information
+      console.log(`Image URL length: ${imageUrl.length}`);
+      const imageSizeKB = (imageUrl.length * (3/4)) / 1024; // Base64 strings use about 3/4 of their length in bytes, subtracting the overhead for the Data URI prefix
+      console.log(`Approximate image size: ${imageSizeKB} KB`);
+  
+      // Check if the size exceeds a certain limit (e.g., 5MB)
+      if (imageSizeKB > 5120) { // 5MB in KB
+        console.error('Image size exceeds 5MB');
+        return '';
       }
+  
+      // Optionally, log a portion of the image data URL to inspect manually (not the whole string as it can be very long)
+      console.log(`Image Data URL Sample: ${imageUrl.substring(0, 100)}`);
+  
+      return imageUrl.replace(/^data:image\/jpeg;base64,/, '');
     }
-    return "";
-  }, []);
+    return '';
+  };
+  
 
-  const describeImage = useCallback(async (imageData: string) => {
+  const describeImage = async (imageData: string) => {
     try {
       const response = await fetch("/api/describe", {
         method: "POST",
@@ -65,22 +65,23 @@ const Home: React.FC = () => {
     } catch (error) {
       console.error("Error sending the image to the server:", error);
     }
-  }, []);
+  };
 
   useEffect(() => {
+    let interval: NodeJS.Timer;
     if (streamStarted) {
-      const interval = setInterval(async () => {
+      interval = setInterval(async () => {
         const imageData = await captureImage();
         if (imageData) {
           await describeImage(imageData);
         }
-      }, 10000); // Captures an image every 10 seconds
-
-      return () => {
-        clearInterval(interval);
-      };
+      }, 10000); // 20 seconds interval
     }
-  }, [streamStarted, captureImage, describeImage]);
+
+    return () => {
+      if (interval) clearInterval(interval);
+    };
+  }, [streamStarted]);
 
   return (
     <main className={styles.main}>
